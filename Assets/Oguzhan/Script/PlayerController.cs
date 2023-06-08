e6e1e4fe8977769b7333ee2811c478594a0c217b
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-06-08 18:19:49 +0300

    Player Test Controller Eklendi

---
+++ Assets/Oguzhan/Script/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private bool IsonGround;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float JumpForce;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+    private Rigidbody rigidbodyPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyPlayer = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsonGround = true;
+    }
+    private void FixedUpdate()
+    {
+        GetMovementInput();
+        GetJumpInput();
+    }
+    private void GetMovementInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+        rigidbodyPlayer.velocity = new Vector3(horizontalInput * speed * Time.deltaTime, rigidbodyPlayer.velocity.y, verticalInput * speed * Time.deltaTime);
+
+
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private void GetJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsonGround)
+        {
+            rigidbodyPlayer.velocity = Vector3.up * JumpForce;
+            IsonGround = false;
+        }
+    }
+}
