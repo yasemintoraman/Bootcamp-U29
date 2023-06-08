36ac437729c00492a9c7f14d0e5dfb477fd66216
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-06-08 21:50:14 +0300

    NPC
    npc oluşturuldu ve üzerine etkileşim için canvas eklendi.
    diyalog sistemi %30 tamamlandı

--- Assets/Oguzhan/Script/GameManager.cs
+++ Assets/Oguzhan/Script/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamFollowPlayer : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -12,11 +12,6 @@ public class CamFollowPlayer : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
-
-    private void LateUpdate()
     {
         
     }
