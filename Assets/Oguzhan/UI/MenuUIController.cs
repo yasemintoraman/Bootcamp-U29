169b8bf17f2b0dc77b6a1e3a07e873b2d14019f5
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-07-12 00:02:40 +0300

    UI%60

--- Assets/Oguzhan/UI/MenuUIController.cs
+++ Assets/Oguzhan/UI/MenuUIController.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuUIController : MonoBehaviour
 {
     public GameObject creditsPopUp;
     public GameObject settingsPopUp;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject settingsPopUpEscMenu;
+    public GameObject ESCPopUp;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && ESCPopUp.active == false)
+        {
+            ESCPopUp.active = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && ESCPopUp.active == true)
+        {
+            ESCPopUp.active = false;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     public void StartButtonPressed()
     {
@@ -36,6 +43,31 @@ public class MenuUIController : MonoBehaviour
 
     public void SettingsButtonPressed()
     {
+        if (settingsPopUp.active == false)
+        {
+            settingsPopUp.active = true;
+        }
+        else
+        {
+            settingsPopUp.active = false;
+        }
+    }
 
+    public void SettingsButtonPressedESCMenu()
+    {
+        if (settingsPopUpEscMenu.active == false)
+        {
+            settingsPopUpEscMenu.active = true;
+        }
+        else
+        {
+            settingsPopUpEscMenu.active = false;
+        }
+    }
+
+
+    public void BackToMenuPressed()
+    {
+        SceneManager.LoadScene(0);
     }
 }
