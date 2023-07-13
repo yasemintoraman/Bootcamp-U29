e03a9cb7f43f4109beb2f8c69a66a5e8f0d25b95
Yasemin Toraman <yaseminntoraman@gmail.com>
2023-07-13 16:28:05 +0300

    enemy, health scripts

---
+++ Assets/Scripts2/AttackSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+[CreateAssetMenu(menuName = "Attacks/Normal Attack")] 
+
+public class AttackSO : ScriptableObject
+{
+    public AnimatorOverrideController animatorOV;
+    public float damage;
+    public float health;
+}
---
+++ Assets/Scripts2/EnemySO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+
+public class EnemySO : ScriptableObject
+{
+    public float healthEnemy;
+    public float tolalHealthEnemy;
+    public float amountofDamage;
+
+}
